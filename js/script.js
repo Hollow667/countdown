98381dfb318279a7a3fd6d71f7d779599c2de309
gibran abdillah <unkownuser145@gmail.com>
2020-09-20 20:56:56 +0700

    add image, change style

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ function hitungMundur(){
         var minute = document.getElementById('menit');
 
 
-        var ultah = new Date('2 Oct 2020');
+        var ultah = new Date('2 Oct 2020 ');
         var Sekarang = new Date();
         var hasil = ultah-Sekarang;
 
@@ -17,17 +17,28 @@ function hitungMundur(){
         var menit = Math.floor(hasil % (1000*3600)/(1000*60));
         var detik = Math.floor((hasil % (1000 * 60)) / 1000);
         //console.log(hari,jam,detik);
-
+        
+        if (jam < 10){
+            var jam = '0' + jam;
+        }
+        if (menit < 10 ){
+            var menit = '0' + menit;
+        }
+        if (detik < 10){
+            var detik = '0' + detik;
+        }
         hour.innerHTML = jam;
         days.innerHTML = hari;
         seconds.innerHTML = detik
         minute.innerHTML = menit;
 
+        
+
         if(detik < 0){
             
             clearInterval(y);
             //console.log('udah');
-            document.getElementById('konten').innerHTML = '<h1>Happy Birthday!</h1>'
+            document.getElementById('konten').innerHTML = '<h1>Happy Birthday!</h1>'; // menampilkan pesan 
         }
 
     },1000) // timeout
